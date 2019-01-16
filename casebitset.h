ddad69c039644090697496ab5e751ac3cf689fc5
manlio <manpuglisi94@gmail.com>
2019-01-16 15:37:05 +0100

    fixed prototype, now working fine

--- casebitset.h
+++ casebitset.h
@@ -10,79 +10,68 @@ string CaseOfBits(string str1, string str2){
     string retString;
     int len = str1.length();
     
+    
     switch (len)
     {
         case 40: {
             bitset<40> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 48: {
             bitset<48> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 56: {
             bitset<56> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 64: {
             bitset<64> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 72: {
             bitset<72> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 80: {
             bitset<80> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 88: {
             bitset<88> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 96: {
             bitset<96> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 104: {
             bitset<104> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 112: {
             bitset<112> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 120: {
             bitset<120> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;
         case 128: {
             bitset<128> bstr1(str1), bstr2(str2), finale;
 	        finale = bstr1 | bstr2; // here it is done the bitwise OR
-            cout << finale;
-            retString = finale.to_string<char, std::string::traits_type, std::string::allocator_type>();
+            retString = to_string(finale.to_ullong());
         } break;    
         default: {
             cout<<"Invalid string.length()\n";
