28d896998e48c55140969ba76fc2f21da0a03121
manlio <manpuglisi94@gmail.com>
2019-01-17 16:45:51 +0100

    now include algorithm header only if using an unix-like OS

--- headers.h
+++ headers.h
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <string>
 #include <bitset>
-#include <algorithm>    // NOT necessary if running on Windows via Visual Studio 2017
-                        // Necessary, instead, if running on Ubuntu or other Unix-like systems
+
+#if defined(__unix__)
+    #include <algorithm>    // NOT necessary if running on Windows via Visual Studio 2017
+                            // Necessary, instead, if running on Ubuntu or other Unix-like systems
+#endif
 
 // I'll use only std methods, so it is safe to use namespace std
 using namespace std;
